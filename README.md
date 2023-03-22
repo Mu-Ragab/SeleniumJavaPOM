ca3198d4ee2234d410fa3f1dd9a59f4c8ff8553b
MuIbrahim23 <mi0822@dexcom.com>
2023-03-22 16:12:45 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # SeleniumJavaPOM
+# QATask-JavaSelenium
--- src/test/java/BaseTests.java
+++ src/test/java/BaseTests.java
@@ -4,10 +4,7 @@ import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.*;
 import pages.HomePage;
 
 public class BaseTests {
@@ -23,7 +20,7 @@ public class BaseTests {
     public void setup() {
         System.setProperty("webdriver.chrome.driver","resources/chromedriver");
         driver = new ChromeDriver();
-        driver.get("https://www.demoblaze.com/");
+        driver.get("https://subscribe.stctv.com/");
         driver.manage().window().maximize();
 
         homePage = new HomePage(driver);
